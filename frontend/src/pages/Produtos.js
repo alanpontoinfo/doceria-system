df2d68c50b3cb6a8fc08d9fef84e788dde225470
Estagiario <alanpontoinfo@gmail.com>
2026-04-10 20:56:14 -0300

    atualizando frontend  do vercel.json

--- frontend/src/pages/Produtos.js
+++ frontend/src/pages/Produtos.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import api from '../api';
+import api from '../api/api';
 import { Trash2, Edit, Plus, FileText, X, ShoppingBag, LogOut, User, DollarSign, ChevronLeft, ChevronRight } from 'lucide-react';
 import './Produtos.css';
 
