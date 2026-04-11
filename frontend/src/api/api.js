9807e71e570423d166c2cd812394467da0aeefb2
Estagiario <alanpontoinfo@gmail.com>
2026-04-10 21:05:47 -0300

    atualizando  o vercel.json

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // Se estiver em produção, usa a URL relativa /api, senão usa o localhost
-  baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000/api'
+  baseURL: process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:5000'
 });
 
 // Interceptor para injetar o ID do usuário em cada requisição
